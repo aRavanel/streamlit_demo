5e33a09c8717b7416ca13862825a04ba090f9cb3
Massimiliano PRONESTI <massimiliano.pronesti@amadeus.com>
2022-11-15 15:57:23 +0100

    feat(wikipedia): add first dummy version

--- pages/4_hidden_wikipedia_page_team2.py
+++ pages/4_hidden_wikipedia_page_team2.py
@@ -1,6 +1,22 @@
 import streamlit as st
-st.title('Insert your page here')
 
+st.title("Guess the Secret Wikipedia Page!")
 
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
 
+col1, col2 = st.columns(2)
 
+with col1:
+    text_input = st.text_input(
+        "Write Your Guess 👇",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled
+    )
+
+st.text("")
+with open('constants/wikipedia.txt') as f:
+    word = f.read()
+    st.write(word)
