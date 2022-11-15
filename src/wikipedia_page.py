89fe058a6df4021606c8d664056d75b5f76fefb3
avranasa <eavranasa@gmail.com>
2022-11-15 16:23:03 +0100

    first version

---
+++ src/wikipedia_page.py
@@ -0,0 +1,52 @@
+text = """
+Transport
+
+Article
+Discussion
+Lire
+Modifier
+Modifier le code
+Voir l’historique
+Page d’aide sur l’homonymie
+Pour les articles homonymes, voir Transport (homonymie).
+
+Si ce bandeau n'est plus pertinent, retirez-le. Cliquez ici pour en savoir plus.
+Cet article sur les transports doit être recyclé (janvier 2014).
+
+Une réorganisation et une clarification du contenu paraissent nécessaires. Améliorez-le, discutez des points à améliorer ou précisez les sections à recycler en utilisant {{section à recycler}}.
+
+Une planche du Larousse universel de 1922 illustrant les modes de transport
+Une planche illustrant les modes de transport (Larousse universel en 2 volumes, 1922)1.
+
+Le réseau routier de transport automobile est confronté aux fleuves et petits bras de mer qui peuvent être traversés par de grands ponts, des tunnels, des bacs ou des ferrys.
+
+L'avion est un mode de transport en très forte croissance depuis la seconde moitié du xxe siècle, mais dont les impacts écologiques et climatiques sont importants (ex. : à raison de 3,15 kg CO2/kg de kérosène, les vols intra-européens émettent environ 80 Mt CO2 par an en 20072.
+
+Le pipe-line permet de transporter rapidement de grandes quantités de liquides et gaz, ici en Alaska.
+
+Un autobus.
+Le transport est le déplacement d'objets, de marchandises, ou d'individus (humains ou animaux) d'un endroit à un autre. Les modes de transport incluent l'aviation, le chemin de fer, le transport routier, le transport maritime, le transport par câble, l'acheminement par pipeline et le transport spatial. Le mode dépend également du type de véhicule ou d'infrastructure utilisé. Les moyens de transport peuvent inclure les véhicules à propulsion humaine, l'automobile, la moto, le scooter, le bus, le métro, le tramway, le train, le camion, la marche à pied, l'ascenseur, l'hélicoptère, le bateau ou l'avion.
+
+Définitions
+Le déplacé peut être : quelque chose de nombrable, dénombrable ou discret (ex. : des conteneurs) ; quelque chose de continu (ex. : matériau extrudé ou fluide) ; ou un animal, un humain ou un groupe d'individus (morts ou vivants). Lors du parcours, les points départ et d'arrivée peuvent être choisis ou imposés par un prestataire du transport. Pour l'arrivée, le mot (« destination ») est plus précis et utilisé quasi uniquement dans ce secteur.
+
+Un mobile, support ou contenant de transport est le plus souvent nécessaire ; il s'agit généralement d'un véhicule, sauf dans les tuyaux de transport par exemple. Des infrastructures lourdes sont toujours nécessaires (port, gares, routes, canaux, ligne de chemin de fer, circuit, piste, etc.). On peut les séparer en catégories : infrastructures de voies de communication, infrastructures de destination, infrastructures de triage et d'intermodalité. Le pilotage peut être individuel, automatique, centralisé, semi-automatique. Le parcours peut-être figé, captif ou prisonnier d'une voie, ou libre (bateau, avion). Il peut également être uni-séquentiel ou multi-séquentiel (il suppose alors des temps imposés d'attente). L'énergie consommée peut être contenue dans le mobile (autonomie énergétique continue ou partielle), semi-autonome, ou dépendant énergétiquement de l'extérieur (cabines de téléphérique, trains électriques, etc). Les combinaisons sont aussi de mise. L'apport énergétique nécessite un réseau énergétique d'approvisionnement.
+
+Dans un même mobile de transport, plusieurs sources d'énergie peuvent être utilisées de manière simultanée (conjointes), séquentielle, alternative, transitoire (accélérations, décélérations). L'énergie cinétique peut faire l'objet de récupérations partielles. L'énergie interne n'est pas toujours utilisable sous forme primaire. Elle nécessite alors un système de conversion d'énergie (ex. : moteur Diesel). L'interface entre voie et mobile est souvent constituée d'une ou de plusieurs roues. Le secteur du transport est une composante économique majeure. Il engloutit à lui seul 32 % de l'énergie totale3 consommée en France en 20114.
+
+Par assimilation, des actions de déplacement et de conduction ont été dénommées « transports », comme le transport d'électricité (qui s'effectue sur des réseaux de câbles électriques), de gaz, de pétrole (au travers de conduites, les pipelines). En ce qui concerne le « transport » d'informations et les télécommunications, il vaut mieux utiliser le mot « transmission »5.
+
+L'article qui suit concerne par conséquent, non seulement le transport per se, mais aussi ce qui conduit (oléoducs, gazoducs, câbles électriques), transmet (les courants forts ou les courants faibles tels que les signaux, messages, informations…), fournit et approvisionne (gaz, électricité, eau, pétrole…)[Lequel ?].
+
+Deux enfants qui se déplace à travers leur quartier à l'aide d'un vélo que chacun prend à tout de rôle
+Deux enfants qui se déplace à travers leur quartier à l'aide d'un vélo que chacun prend à tout de rôle
+Les voies de communication font partie des infrastructures et réseaux de transport, comme les ouvrages d'art (ponts, tunnels..) et les bâtiments (gares, parkings…) associés. Elles contribuent au phénomène dit de fragmentation écopaysagère que la Trame verte et bleue cherche à compenser en France. Le transport motorisé est par ailleurs une des sources majeures de pollution ; par exemple, en France, il représente environ 30 % des émissions de gaz à effet de serre6.
+
+Enjeux
+
+Le transport routier, encore en développement dans une partie du monde, est déjà très utilisé ailleurs. Il est source de pollution, d'intenses émissions de gaz à effet de serre, de fragmentation, d'accidents, mais aussi confronté à certaines limites engorgements, empreinte écologique, périurbanisation.
+Le transport est un service (public ou privé selon les cas) nécessaire ou utile pour de nombreux actes et activités de la vie courante. Le type de transport et son caractère plus ou moins intermodal ont des conséquences en matière de consommation d'espace et d'énergie, ainsi qu'en matière d'émissions de polluants et de gaz à effet de serre (et donc en matière de santé environnementale). Il est aussi devenu un secteur économique lié à l'industrie du transport qui s'est développée simultanément dans les domaines public et privé depuis la révolution industrielle. Ce développement a contribué au phénomène de mondialisation, ainsi qu'au développement du tourisme de destinations lointaines.
+
+Le transport est un enjeu stratégique majeur, fragilisé par la montée rapide des coûts énergétiques et la raréfaction de certaines ressources (foncières notamment). Les investissements énormes qui y sont liés, sont à mettre en perspective des services rendus dans le modèle économique global où la mobilité joue un rôle majeur. Les transitions énergétiques attendues semblent imposer avant tout une utilisation économe des ressources, ce qui implique d'inventer des transports du futur innovants, aux rendements globaux améliorés7, pour ne pas mettre en péril jusqu'aux ressources nécessaires à la transition (on parle[Qui ?] de « transport intelligent » et « dé-carboné »). Un des enjeux est sa durabilité qui implique aussi une opinion publique informée et consciente des enjeux globaux et locaux du transport\
+"""
+
