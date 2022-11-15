13c32c669d46879b0d1a1b9920f6073544a7a2b2
Giulio Corallo <s282380@studenti.polito.it>
2022-11-15 16:13:24 +0100

    load word2vec model, calculate temperature
    * add load word2vec model function
    * add calculate temperature function
    * add gensim requirement to requirements.txt

--- src/utils_nlp.py
+++ src/utils_nlp.py
@@ -1,5 +1,28 @@
-def split_string(somestring):
+import gensim
+import os
+import pathlib
+
+
+def load_model():
     '''
-    for testing puprose
+    load the word2vec model
     '''
-    return somestring.split(' ')
+    current_path = os.path.abspath(__file__) # path of the file
+    path_obj = pathlib.Path(current_path) # lets go above
+    path_model = str(path_obj.parent.parent) + '/models/GoogleNews-vectors-negative300-SLIM.bin' # add model path
+    #print('path of the model : ', path_model)
+    model = gensim.models.KeyedVectors.load_word2vec_format(path_model, binary=True)
+    return model
+
+def calculate_temperature(model, candidate, reference):
+    '''
+    function to compute similarity score
+    from resources.inspiration.calling_w2v import call_w2v
+    print(call_w2v('bread', 'butter'))
+    '''
+    result = model.similarity(candidate, reference) # note model.wv.similarity(word1, word2) is deprecated\
+    temperature = result * 100
+    return temperature
+
+
+
